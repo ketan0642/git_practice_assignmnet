ae1c9c0d4af9d871993053b6474b529c073977a7
Jayshri3010 <jayshribkor30@gmail.com>
2022-11-02 10:41:38 +0530

    fixing code for prime and palindrome problem

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,21 @@
-let N=5 and str= "ketan"
+let N=5  
+let str= "ketan"
 function checkPalindrome(N, str) {
-    //write code here
+    let bag1="";
+    let bag2="";
     
-   
+    for(let i=0; i<=str.length-1; i++)
+{
+        bag1 += str[i];
+    }for(let i=str.length-1; i>=0; i--)
+{
+        bag2 += str[i];
+    } 
+    if (bag1==bag2)
+{
+        console.log("Yes");
+    }else
+{
+        console.log("No");
+}   
 }
